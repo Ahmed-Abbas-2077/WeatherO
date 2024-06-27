6a9ddd4c8f837a76581aa7ec84859a94f301d0e5
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2024-06-27 21:44:28 +0300

    fetch weather & forecast

--- WeatherO/weatherApp/views.py
+++ WeatherO/weatherApp/views.py
@@ -21,6 +21,42 @@ def index(request):
         city1 = request.POST['city1']
         city2 = request.POST.get('city2', None)  # get city2 from the form
 
-        # deal with data somehow
     else:
         return render(request, 'weatherApp/index.html')
+
+
+# This function fetches the current weather and 5-day forecast of the city (not a route handler)
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+
+    # A json response with the city's weather data
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+
+    # lat and lon are the latitude and longitude of the city, respectively
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+
+    # A json response with the city's 5-day forecast data
+    forecast_response = requests.get(
+        forecast_url.format(lat, lon, api_key)).json()
+
+    # Extracting the required data from the json responses
+    weather_data = {
+        'city': city,
+        # converting temperature from Kelvin to Celsius. 2 is the number of decimal places
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+    daily_forecasts = []
+    for daily_data in forecast_response['daily'][:5]:
+        daily_forecasts.append({
+            # converting timestamp to day. strftime is used to format the date into a readable form
+            'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+            # converting temperature from Kelvin to Celsius. 2 is the number of decimal places
+            'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+            'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+            'description': daily_data['weather'][0]['description'],
+            'icon': daily_data['weather'][0]['icon'],
+        })
+
+    return weather_data, daily_forecasts
